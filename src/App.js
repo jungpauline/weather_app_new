e2ebd311db7a427f1e7c823d5645dd252372e9ef
jungpauline <54990995+jungpauline@users.noreply.github.com>
2023-04-01 11:06:01 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <div>Weather App</div>
+        <footer>
+          This project was coded by Pauline Jung and is on
+          <a
+            href="https://github.com/jungpauline/weather_app_new"
+            target="_blank"
+            rel="noreferrer">
+            {" "}
+            GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
