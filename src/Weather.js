7ffeafef63b6b37207eb38ec2cf28192de544429
jungpauline <54990995+jungpauline@users.noreply.github.com>
2023-04-02 14:54:38 +0200

    Added day and time

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Weather.css";
+import FormattedDate from "./FormattedDate";
 import axios from "axios";
 
 export default function Weather(props) {
@@ -15,7 +16,7 @@ export default function Weather(props) {
       description: response.data.condition.description,
       feelsLike: response.data.temperature.feels_like,
       icon: response.data.condition.icon_url,
-      date: "Wednesday 07:00",
+      date: new Date(response.data.time * 1000),
     });
   }
 
@@ -43,7 +44,9 @@ export default function Weather(props) {
         </form>
         <h1>{weatherData.city}</h1>
         <ul>
-          <li>{weatherData.date}</li>
+          <li>
+            <FormattedDate date={weatherData.date} />
+          </li>
           <li className="text-capitalize">{weatherData.description}</li>
         </ul>
         <div className="row mt-3">
