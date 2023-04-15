26894ed850e0dd897a46743bcf7698fb40b279b1
jungpauline <54990995+jungpauline@users.noreply.github.com>
2023-04-15 11:48:10 +0200

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -8,14 +8,29 @@ function App() {
       <div className="container">
         <Weather defaultCity="Lisbon" />
         <footer>
-          This project was coded by Pauline Jung and is on
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/pauline-jung-8a0515150/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Pauline Jung
+          </a>{" "}
+          and is on{" "}
           <a
             href="https://github.com/jungpauline/weather_app_new"
             target="_blank"
             rel="noreferrer"
           >
-            {" "}
             GitHub
+          </a>{" "}
+          , hosted on{" "}
+          <a
+            href="https://friendly-cocada-015779.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify.
           </a>
         </footer>
       </div>
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,6 +17,15 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
+  function load() {
+    let apiKey = "420837090555f231450d0bd2abtbeaao";
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
   if (loaded) {
     return (
       <div className="WeatherForecast">
@@ -28,18 +37,15 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
-    let apiKey = "420837090555f231450d0bd2abtbeaao";
-    let longitude = props.coordinates.longitude;
-    let latitude = props.coordinates.latitude;
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
-
-    axios.get(apiUrl).then(handleResponse);
+    load();
 
     return null;
   }
